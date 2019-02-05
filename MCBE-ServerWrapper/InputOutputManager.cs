6c647f1ff307350b7a6e88cd5125047348ae35ed
Joel Ahlgren <joel@ahlgren.io>
2019-02-06 00:07:57 +0100

    Cleaned up debugging code a bit, added delay to welcome message so that joinee can catch it too.

--- MCBE-ServerWrapper/InputOutputManager.cs
+++ MCBE-ServerWrapper/InputOutputManager.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Diagnostics;
     using System.Text.RegularExpressions;
+    using System.Threading;
 
     /// <summary>
     /// 
@@ -36,91 +37,64 @@
             if (e.Data.Contains("Starting Server"))
             {
                 _serverStarting = DateTime.Now;
-                Console.WriteLine("Server starting...");
-                return;
             }
 
             if (e.Data.Contains("Server started"))
             {
-                if (_serverStarting == DateTime.MinValue)
-                {
-                    Console.WriteLine("Server started.");
-                }
-                else
+                if (_serverStarting != DateTime.MinValue)
                 {
                     Console.WriteLine($"Server started in {(DateTime.Now - _serverStarting).TotalMilliseconds} ms.");
+                    return;
                 }
-
-                return;
             }
 
             if (e.Data.Contains("Player connected"))
             {
-                var playerName = Regex.Match(e.Data, $".* Player connected: (.*), ").Groups[1].Value;
-                Console.WriteLine($"Player \"{playerName}\" connected.");
-                _serverProcess.Say($"Welcome {playerName}!");
+                var playerName = Regex.Match(e.Data, @".* Player connected: (.*), ").Groups[1].Value;
 
-                return;
+                var thread = new Thread(() =>
+                {
+                    Thread.Sleep(5000);
+                    _serverProcess.Say($"Welcome {playerName}!");
+
+                });
+                thread.Start();
             }
 
             if (e.Data.Contains("Player disconnected"))
             {
-                var playerName = Regex.Match(e.Data, $".* Player disconnected: (.*), ").Groups[1].Value;
-                Console.WriteLine($"Player \"{playerName}\" disconnected.");
+                var playerName = Regex.Match(e.Data, @".* Player disconnected: (.*), ").Groups[1].Value;
                 _serverProcess.Say($"Goodbye {playerName}!");
-
-                return;
             }
 
             if (e.Data.Contains("Difficulty: ") && !_serverProcess.ServerValues.ContainsKey("Difficulty"))
             {
-                var difficulty = Regex.Match(e.Data, @".*Difficulty: \d (.*)").Groups[1].Value;
-                _serverProcess.ServerValues.Add("Difficulty", difficulty);
-                Console.WriteLine($"Difficulty: {difficulty}.");
-
-                return;
+                _serverProcess.ServerValues.Add("Difficulty", Regex.Match(e.Data, @".*Difficulty: \d (.*)").Groups[1].Value);
             }
 
             if (e.Data.Contains("Game mode: ") && !_serverProcess.ServerValues.ContainsKey("GameMode"))
             {
-                var gameMode = Regex.Match(e.Data, @".*Game mode: \d (.*)").Groups[1].Value;
-                _serverProcess.ServerValues.Add("GameMode", gameMode);
-                Console.WriteLine($"Game mode: {gameMode}.");
-
-                return;
+                _serverProcess.ServerValues.Add("GameMode", Regex.Match(e.Data, @".*Game mode: \d (.*)").Groups[1].Value);
             }
 
             if (e.Data.Contains("Level Name: ") && !_serverProcess.ServerValues.ContainsKey("LevelName"))
             {
-                var levelName = Regex.Match(e.Data, @".*Level Name: (.*)").Groups[1].Value;
-                _serverProcess.ServerValues.Add("LevelName", levelName);
-                Console.WriteLine($"Level name: {levelName}.");
-
-                return;
+                _serverProcess.ServerValues.Add("LevelName", Regex.Match(e.Data, @".*Level Name: (.*)").Groups[1].Value);
             }
 
             if (e.Data.Contains("Version") && !_serverProcess.ServerValues.ContainsKey("ServerVersion"))
             {
                 _serverProcess.ServerValues["ServerVersion"] = Regex.Match(e.Data, @".*Version (\d\.\d\.\d\.\d)").Groups[1].Value;
-                Console.WriteLine($"Server version: {_serverProcess.ServerValues["ServerVersion"]}");
-
-                return;
             }
 
             if (e.Data.Contains("IPv4 supported") && !_serverProcess.ServerValues.ContainsKey("IpV4Port"))
             {
                 _serverProcess.ServerValues["IpV4Port"] = Regex.Match(e.Data, @".*port: (\d*)").Groups[1].Value;
-                Console.WriteLine($"Server IPv4 port: {_serverProcess.ServerValues["IpV4Port"]}");
-
-                return;
             }
 
             if (e.Data.Contains("IPv6 supported") && !_serverProcess.ServerValues.ContainsKey("IpV6Port"))
             {
                 _serverProcess.ServerValues["IpV6Port"] = Regex.Match(e.Data, @".*port: (\d*)").Groups[1].Value;
-                Console.WriteLine($"Server IPv6 port: {_serverProcess.ServerValues["IpV6Port"]}");
-
-                return;
             }
 
             Console.WriteLine(e.Data);
