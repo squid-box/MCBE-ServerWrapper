f646b7bd39a1c2f19590f7f85add510b82557f6c
Joel Ahlgren <joel@ahlgren.io>
2021-02-28 01:05:05 +0100

    Reworked GitLab CI definition.

--- MCBE-ServerWrapper/PlayerManagement/PlayerManager.cs
+++ MCBE-ServerWrapper/PlayerManagement/PlayerManager.cs
@@ -3,7 +3,9 @@
     using System;
     using System.Collections.Generic;
     using System.IO;
+
     using AhlSoft.BedrockServerWrapper.Logging;
+
     using Newtonsoft.Json;
 
     /// <inheritdoc cref="IPlayerManager" />
